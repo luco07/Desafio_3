12d09c03925973c8808c4e66aa44023de96f695a
luciano sosa <sosasipos@gmail.com>
2023-05-18 09:39:43 -0300

    Desafio_3

---
+++ CursoAp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello Word</h1>
+      <p>hola mi rey</p>
+    </>
+  )
+}
+
+export default App
